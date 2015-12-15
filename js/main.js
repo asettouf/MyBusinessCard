fbf901746edb45d56b4d606be672a38053bb6af1
asettouf <adonis.settouf@gmail.com>
2015-12-15 16:37:11 +0100

    Fixing several bugs, adding the reading section

--- js/main.js
+++ js/main.js
@@ -6,17 +6,30 @@ $(document).ready(function(){
  * init the index.html
  */
 var init = function(){
-  loadText("reception.html");
+  loadMainHtml("reception.html");
   createHandlersForTopBar();
+  handleReading();
 }
+
+/**
+ *load main html, the reception with a category and the skills section
+ *
+ */
+var loadMainHtml = function(target){
+  loadHtml("reception", target);
+  loadHtml("main", "skills.html");
+}
+
 /**
  * load html from a page to the main contener
+ * @param  {[string]} id [id of the tag to load html into]
  * @param  {[string]} htmlPage [name of the html page to retrieve data]
  */
-var loadText = function(htmlPage){
+var loadHtml = function(id, htmlPage){
   $.get(htmlPage)
     .done(function(data){
-      $("#reception").html(data);
+      //console.log(data);
+      $("#" + id).html(data);
   }).error(function(data){
     console.log(data);
   });
@@ -31,7 +44,19 @@ var createHandlersForTopBar = function(){
     var target = $(this).attr("src");
     $(".active").toggleClass("active");
     $(this).toggleClass("active");
-    loadText(target);
+    $("#myIntro").show();
+    loadMainHtml(target);
   });
+}
 
+/**
+ * Handle the special case of click on readings
+ */
+var handleReading = function(){
+  $("#reading").click(function(){
+    $("#myIntro").hide();
+    $(".active").toggleClass("active");
+    $(this).toggleClass("active");
+    loadHtml("main", "readings.html");
+  });
 }
