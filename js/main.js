0f7a9caab8c850a452288fb35bd8e6c3a0aeb1f4
asettouf <adonis.settouf@gmail.com>
2015-12-15 15:22:01 +0100

    Adding html pages, ajax js calls, and media query in less

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
-  loadText("reception.html");
+  init();
 })
 
+/**
+ * init the index.html
+ */
+var init = function(){
+  loadText("reception.html");
+  createHandlersForTopBar();
+}
+/**
+ * load html from a page to the main contener
+ * @param  {[string]} htmlPage [name of the html page to retrieve data]
+ */
 var loadText = function(htmlPage){
   $.get(htmlPage)
     .done(function(data){
@@ -11,6 +22,16 @@ var loadText = function(htmlPage){
   });
 }
 
-var cickTopElementHandler = function(){
-    
+/**
+ * Creates the click listeners on the top li tags
+ *
+ */
+var createHandlersForTopBar = function(){
+  $(".toRetrieve").click(function(){
+    var target = $(this).attr("src");
+    $(".active").toggleClass("active");
+    $(this).toggleClass("active");
+    loadText(target);
+  })
+
 }
