4719cd84d19a7aaa2ed100c0ced515e42b40fe26
asettouf <adonis.settouf@gmail.com>
2015-12-18 12:10:51 +0100

    Adding swipe handler

--- js/main.js
+++ js/main.js
@@ -2,10 +2,14 @@ $(document).ready(function(){
   init();
 });
 
+//TODO: build li array, activate the swipe with active
+
 //@Param {int} current Position in the tabs for swiping purpose
 var currentPosition = 0;
 //@Param {array} array containing pages to load in ajax
 var pagesArray = ["reception.html", "educ.html", "career.html"];
+//@Param {array} array containing pages to load in ajax
+var liIds = ["about", "education", "career"];
 
 /**
  * init the index.html
@@ -13,8 +17,9 @@ var pagesArray = ["reception.html", "educ.html", "career.html"];
 var init = function(){
   loadMainHtml("reception.html");
   createHandlersForTopBar();
-  handleReading();
+  handleSwiping();
 };
+
 
 /**
  *load main html, the reception with a category and the skills section
@@ -39,11 +44,67 @@ var loadHtml = function(id, htmlPage){
   });
 };
 
+
 /**
  * Handle swipes from a mobile device
  */
 var handleSwiping = function(){
+  $( "#reception" ).on( "swiperight", swipeRightHandler );
+  $( "#reception" ).on( "swipeleft", swipeLeftHandler );
+}
 
+var checkCurrentPosition = function(){
+  var pageName = $(".active")[0].id;
+  var res = 0;
+  var i = 0;
+  console.log(pageName);
+  for (i; i<liIds.length; i++){
+    res = (pageName === liIds[i] ? i: 0);
+    console.log(liIds[i]);
+  }
+  return res;
+}
+/**
+ * Handles swipe right
+ * @param  {Javascript event} event The triggered event on swipe right
+ */
+var swipeRightHandler = function(event){
+  currentPosition = checkCurrentPosition();
+  if (currentPosition > 0){
+      currentPosition--;
+      toogleActiveLiTop($("#" + liIds[currentPosition]));
+      loadMainHtml(pagesArray[currentPosition]);
+  } else if(currentPosition == 0){
+    console.log("Nothing right  ");
+  } else{
+    console.log("An error occured");
+  }
+}
+
+/**
+ * Handles swipe left
+ * @param  {Javascript event} event The triggered event on swipe left
+ */
+var swipeLeftHandler = function(event){
+    currentPosition = checkCurrentPosition();
+  if (currentPosition < pagesArray.length - 1){
+      currentPosition++;
+      toogleActiveLiTop( $("#" +liIds[currentPosition]));
+      loadMainHtml(pagesArray[currentPosition]);
+  } else if(currentPosition == pagesArray.length - 1){
+    console.log("Nothing left");
+  } else{
+    console.log("An error occured");
+  }
+}
+
+/**
+ * toogle the current active li when changing content
+ * @param  {jQuery element} element the element to switch to active
+ */
+var toogleActiveLiTop = function(element){
+  $(".active").toggleClass("active");
+  element.toggleClass("active");
 }
 
 /**
@@ -53,8 +114,7 @@ var createHandlersForTopBar = function(){
   $(".toRetrieve").click(function(ev){
     ev.preventDefault();
     var target = $(this).attr("data-src");
-    $(".active").toggleClass("active");
-    $(this).toggleClass("active");
+    toogleActiveLiTop($(this));
     $("#myIntro").show();
     loadMainHtml(target);
   });
